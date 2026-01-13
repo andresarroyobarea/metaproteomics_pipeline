261591e766f70545bf7e4b1b6cd75768ace3cc21
Andres <andresarroyobarea@gmail.com>
2026-01-13 09:40:32 +0100

    Initial state of global master metaproteomics pipeline

---
+++ code/pipeline/run_metaproteomics_pipeline.R
@@ -0,0 +1,9 @@
+# --------------------------------------------------------------------------------------------------------
+# Script: run_metaproteomics_pipeline.R
+# Description: Complete Metaproteomics Data Analysis Pipeline
+# Author: Andrés Arroyo Barea
+# Date: 2026-01-13
+#       Purpose: Generate a pipeline to orchestrate the whole metaprotemomics 
+#       data analysis
+# --------------------------------------------------------------------------------------------------------
+source("code/pipeline/run_preprocessing.R")
