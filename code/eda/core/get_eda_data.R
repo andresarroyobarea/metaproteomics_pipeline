f58619f360447c00e60ab5826dbda50dd7e0df35
Andres <andresarroyobarea@gmail.com>
2026-01-28 12:37:17 +0100

    Add transformation steps in independent functions

--- code/eda/core/get_eda_data.R
+++ code/eda/core/get_eda_data.R
@@ -25,7 +25,13 @@ get_eda_data <- function(
   biological_level,
   metric_name,
   filter_subset,
-  normalization = "none",
+  eda_mode = list(
+    biological_mode = "raw",        # raw | abundance
+    zero_strategy   = "keep",       # keep | na | pseudo
+    transform       = "none",       # none | log2 | log10
+    normalization   = "none",       # none | median
+    post_zero       = "keep"        # keep | zero
+  ),
   eda_verbose = TRUE
 ) {
   
@@ -46,7 +52,7 @@ get_eda_data <- function(
   df <- df %>% dplyr::select(feature_id, matches(paste0("ID_\\d+_", metric_name, "$")))
  
   # 4. Apply normalization.
-  df <- normalize_metric(df, metric_col = metric_name, method = normalization)
+  df <- apply_data_transform(df, metric_col = metric_name, method = normalization)
   
   # Filter sample in case it was selected in eda config.
   # al mismo tiempo que se extraen las muestras?
@@ -54,8 +60,7 @@ get_eda_data <- function(
   return(df)
 }
 
-log10(0)
-eda_metrics
+
 
 # --------------------------------------------------------------------------------------------------------
 #' apply_filter_subset
@@ -141,14 +146,12 @@ apply_feature_filtering <- function(
 #'
 #' @export
 #' # --------------------------------------------------------------------------------------------------------
-normalize_prot_metric <- function(
-    feature_data_list,
+apply_data_transform <- function(
+    df,
     biological_level,
-    normalization,
+    transform_mode = "process",
     verbose = TRUE
 ) {
-  # 1. Select dataframe by biological level.
-  df <- feature_data_list[[biological_level]]
   
   if(is.null(df)) {
     stop("[ERROR]: Biological level not found", biological_level)
@@ -160,9 +163,208 @@ normalize_prot_metric <- function(
     return(df)
   }
   
+  # 3. Not further processing
+  if (transform_mode == "raw") {
+    if (verbose) message("[INFO]: No transformation applied.")
+    return(df)
+  }
+  
+  # 4. Absence/presence
+  if (transform_mode == "abundance") {
+    df_abs_pres <- df %>% mutate_all(~ ifelse(. > 0, 1, 0))
+    if (verbose) message("[INFO]: Data was converted into absence/presence.")
+    return(df_abs_pres)
+  }
+  
+  # 5. Processing : Log2 or Log2 normalization.
+  if (transform_mode == "process") {
+    
+    
+    
+    return(df)
+  }
+  
+  
+  
 
+}
+
+
+
+# --------------------------------------------------------------------------------------------------------
+#' apply_zero_handling
+#'
+#' Description
+#' 
+#' @param df 
+#'    
+#' @param strategy 
+#'    
+#' @param pseudocount
+#' 
+#' @return data.frame
+#'    
+#' @examples
+#' 
+#'
+#' @export
+#' # --------------------------------------------------------------------------------------------------------
+
+apply_zero_handling <- function(df, strategy = "none", pseudocount = 1) {
+  
+  # Not change
+  if (strategy == "none") {
+    message("[INFO]: Zeros were ketp as zeros.")
+    return(df)
+  }
+  
+  # Change 0 to NA
+  if (strategy == "na") {
+    df <- df %>% mutate(across(where(is.numeric), ~ ifelse(. == 0, NA, .)))
+    message("[INFO]: Zeros were turned into NA previous log transformation.")
+    return(df)
+  }
+  
+  # Add pseudocount if needed
+  if (strategy == "pseudocount") {
+  # TODO: Add different zero count strategy for intensities and spectral counts.
+    message(paste0("[INFO]: A pseudocount of", pseudocount,  "was added previous log transformation."))
+    df <- df %>% mutate(across(where(is.numeric), ~ . + pseudocount))
+    return(df)
+  }
+  
+  stop("[ERROR]: Unknown strategy: ", strategy)
+}
+
+# --------------------------------------------------------------------------------------------------------
+#' apply_log_transform
+#'
+#' Description
+#' 
+#' @param df 
+#'    
+#' @param transform 
+#' 
+#' @return data.frame
+#'    
+#' @examples
+#' 
+#'
+#' @export
+#' # --------------------------------------------------------------------------------------------------------
+
+
+apply_log_transform <- function(df, transform = "none") {
+  
+  # No transformation
+  if (transform == "none") {
+    message("[INFO]: NO log transformation was applied.")
+    return(df)
+  }
+  
+  # Log transformations
+  if ((transform == "log2" | transform == "log10") & any(df == 0,na.rm = T)) {
+    message("[WARNING] Log transformation will be applied over 0: Inf values were returned.")
+  }
+  
+  # Log2 transformation  
+  if (transform == "log2") {
+    df <- df %>% mutate(across(where(is.numeric), ~ log2(.)))
+    message("[INFO]: Log2 transformation was applied.")
+    return(df)
+    }
+    
+  # Log10 transformation
+  if (transform == "log10") {
+    df <- df %>% dplyr::mutate(across(where(is.numeric), ~ log10(.)))
+    message("[INFO]: Log10 transformation was applied.")
+    return(df)
+  }
+  
+  stop("Unknown transform: ", transform)
+}
+
+# --------------------------------------------------------------------------------------------------------
+#' apply_normalization
+#'
+#' Description
+#' 
+#' @param df 
+#'    
+#' @param norm_mode 
+#' 
+#' @return data.frame
+#'    
+#' @examples
+#' 
+#'
+#' @export
+#' # --------------------------------------------------------------------------------------------------------
+apply_normalization <- function(df, norm_mode = "median") {
+  
+  # TODO: Add quantile normalization with preprocessCore R package.
+  
+  # No Normalization
+  if (norm_mode == "none"){
+    message("[INFO]: NO normalization was applied.")
+    return(df)
+  }
+  
+  if (any(df == -Inf | df == Inf, na.rm = T)) {
+    stop("[ERROR]: numeric columns contain Inf or -Inf. This usually happens if you log-transformed zeros. Replace zeros with a small value or NA before normalizing.")
+  }
+  
+  # Median normalization.
+  if (norm_mode == "median"){
+    df_norm <- df %>% dplyr::mutate(across(where(is.numeric), ~ . - median(., na.rm = TRUE)))
+    message("[INFO]: Median normalization was applied.")
+    return(df_norm)
+  }
+  
+  stop("Unknown normalization mode: ", norm_mode)
   
 }
+
+# --------------------------------------------------------------------------------------------------------
+#' apply_post_zero_handling
+#'
+#' Description
+#' 
+#' @param df 
+#'    
+#' @param strategy 
+#' 
+#' @return data.frame
+#'    
+#' @examples
+#' 
+#'
+#' @export
+#'
+#'--------------------------------------------------------------------------------------------------------
+apply_post_zero_handling <- function(df, strategy = "none"){
+  
+  # Sometimes zeros or NA were needed for different purposes after normalization.
+  
+  # Zeros retained
+  if (strategy == "none") {
+    message("[INFO]: NO post-zero handling after normalization.")
+    return(df) 
+  }
+  
+  # NA -> ZERO
+  if (strategy == "zero" & any(is.na(df))) {
+    df <- df %>% mutate(across(where(is.numeric), ~ ifelse(is.na(.), 0, .)))
+    message("[INFO]: NAs were turned into zeros")
+    return(df)
+  }
+  
+  stop("Unknown post-zero handling mode: ", norm_mode)
+  
+}
+
+
+
 
 
 
