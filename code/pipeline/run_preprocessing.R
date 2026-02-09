c2cc7796c479087e618cad9c8553095994a5535a
Andres <andresarroyobarea@gmail.com>
2026-02-09 09:41:04 +0100

    Add metaproteomics feature list building

--- code/pipeline/run_preprocessing.R
+++ code/pipeline/run_preprocessing.R
@@ -37,3 +37,33 @@ if (run_metadata) {
   source("code/preprocess/05_metadata_preprocess.R")
   message("[INFO]: Metadata preprocessing completed!")
 }
+
+message(">>> Building feature data list...")
+
+metap_feature_data <- list()
+
+if (run_peptide) {
+  metap_feature_data$peptide <- peptides_processed
+}
+
+if (run_protein) {
+  metap_feature_data$protein <- proteins_processed
+}
+
+if (run_taxonomy) {
+  metap_feature_data$taxonomy <- taxonomy_processed
+}
+
+if (run_functional) {
+  metap_feature_data$functional <- functional_processed
+}
+
+if (length(metap_feature_data) == 0) {
+  stop("[ERROR]: No feature table processed at any level")
+}
+
+message("[INFO]: Feature data list built.")
+message(sprintf("[INFO]: Biological veles available: %s",
+                paste(names(metap_feature_data), collapse = ",")))
+
+message("[INFO]: Preprocessing finished.")
