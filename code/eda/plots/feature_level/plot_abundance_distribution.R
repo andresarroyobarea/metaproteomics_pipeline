f092615b9f915fd1fd73ef75478fd8b6bf967ea8
Andres <andresarroyobarea@gmail.com>
2026-02-16 10:38:44 +0100

    Add iteration over different conditions, update logging information and add colour scale

--- code/eda/plots/feature_level/plot_abundance_distribution.R
+++ code/eda/plots/feature_level/plot_abundance_distribution.R
@@ -8,10 +8,12 @@
 
 plot_abundance_distribution <- function(
     data_long,
+    biological_level = biological_level,
+    metric = metric,
     log_base = 2,
     group_by_sample = F,
     group_by_condition = F,
-    condition_var = "condition", # Importante porque podríamos tener varias comparaciones.
+    condition_vars = c("condition"), # Importante porque podríamos tener varias comparaciones.
     plot_feature_means = F,
     path_results_eda = path_results_eda,
     save_plots = TRUE,
@@ -22,36 +24,37 @@ plot_abundance_distribution <- function(
   # -------------------------------
   # 4.1 Feature abundance global
   # -------------------------------
-  if (verbose) log_info("4.1. Generating overall abudance metric plot...")
+  if (verbose) log_info(paste0("4.1. Generating overall abudance, ",  metric, "plot..."))
   p_feature <- ggplot(data_long, aes(x = value)) +
-    geom_histogram(bins = 50, fill = "#1f77b4", color = "white") +
+    geom_histogram(bins = 50, fill = "skyblue", color = "black") +
     labs(
       title = "Feature Abundance Distribution - Overall",
-      x = paste0("log", log_base, " (", "metric", ")"),
+      x = paste0(log_base, " (", metric, ")"),
       y = "Number of features"
     )
   
   plots$overall <- plot_and_save(
     p_feature,
-    paste0("metric", "_distribution_overall"),
+    paste0(metric, "_distribution_overall"),
     file.path(path_results_eda, "feature_centric", biological_level),
     save_plots
   )
   
   # 4.2 --- Feature abundance distribution by sample --- 
   if (group_by_sample) {
-    log_info("4.2. Generating overall abudance metric plot at sample level...")
+    log_info(paste0("4.2. Generating overall abudance ", metric, " plot at sample level..."))
     p_sample <- ggplot(data_long, aes(x = value, color = sample_id)) +
-      geom_density(na.rm = TRUE, alpha = 0.3) +
+      geom_density(na.rm = TRUE, alpha = 0.3, width = 1) +
       labs(
         title = "Feature Abundance Distribution - Sample level",
-        x = paste0("log", log_base, " (", "metric", ")"),
+        x = paste0(log_base, " (", metric, ")"),
         y = "Density"
-      )
+      ) +
+      scale_color_paletteer_d("rcartocolor::Antique")
     
     plots$per_sample <- plot_and_save(
       p_sample,
-      paste0("metric", "_distribution_per_sample"),
+      paste0(metric, "_distribution_per_sample"),
       file.path(path_results_eda, "sample_centric", biological_level),
       save_plots
     )
@@ -59,21 +62,30 @@ plot_abundance_distribution <- function(
   
   # 4.3 --- Feature abundance distribution by condition ---
   if (group_by_condition) {
-    log_info("4.3. Generating overall abudance metric plot at condition level...")
-    p_condition <- ggplot(data_long, aes(x = value, color = condition)) +
-      geom_density(na.rm = TRUE) +  
-      labs(
-        title = "Feature Abundance Distribution - Condition level",
-        x = paste0("log", log_base, " (", "metric", ")"),
-        y = "Density"
-      )
-    
-    plots$per_condition <- plot_and_save(
-      p_condition,
-      paste0(metric, "_distribution_per_condition"),
-      file.path(path_results_eda, "condition_centric", biological_level),
-      save_plots
-    )
+    for (cond in condition_vars) {
+      
+      log_info(paste0("4.3. Generating overall abudance ", metric, " plot for ", cond, " level..."))
+      
+      p_condition <- ggplot(
+        data_long, 
+        aes(x = value, color = .data[[cond]])
+        ) +
+        geom_density(na.rm = TRUE, alpha = 0.3, width = 1) +  
+        labs(
+          title = paste0("Feature Abundance Distribution - ", cond),
+          x = paste0(log_base, " (", metric, ")"),
+          y = "Density"
+        ) +
+        scale_color_paletteer_d("rcartocolor::Antique")
+      
+      plots[[paste0("per_condition_", cond)]] <- plot_and_save(
+        p_condition,
+        paste0(metric, "_distribution_per_", cond),
+        file.path(path_results_eda, "condition_centric", biological_level),
+        save_plots
+      ) 
+    }
+  
   }
   
   # 4.4 Feature means plot.
@@ -84,10 +96,10 @@ plot_abundance_distribution <- function(
       dplyr::summarise(mean_abundance = mean(value, na.rm = T)) 
     
     p_feature_means <- ggplot(feature_summary, aes(x = mean_abundance)) +
-      geom_histogram(bins = 50, fill = "#1f77b4", color = "white") +
+      geom_histogram(bins = 50, fill = "skyblue", color = "black") +
       labs(
         title = "Feature Abundance Distribution",
-        x = paste0("Feature-mean ", "log(", log_base, ") ", "metric"),
+        x = paste0("Feature-mean (",  log_base, "-", metric, ")"),
         y = "Number of features"
       ) 
     
